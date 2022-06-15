efe7fbde9bb45331315ad529eeed34da3da4dae1
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-06-15 13:36:20 +0600

    add a new project

--- src/component/MyProjects/MyProjects.js
+++ src/component/MyProjects/MyProjects.js
@@ -7,15 +7,15 @@ const MyProjects = () => {
     return (
         <div className="bg-dark text-white">
             <Container className="py-5">
-                <Row >
+                <Row>
                     <div className="d-inline-flex justify-content-center mt-4">
                         <h2 className=" p-3  rounded-pill text-white" style={{ backgroundColor: "#22577E" }}>My Most Recent <span style={{ color: "#ec255a" }}>Projects</span> </h2>
                     </div>
                     <Col className="d-flex my-5 project-media" data-aos="fade-up"
                         data-aos-anchor-placement="center-bottom">
                         <Col>
-                            <div class="card text-dark card-media" style={{ width: "18rem", height: "600px" }}>
-                                <img src="https://i.ibb.co/ssq8Fvc/v-Fa-X5x-F98q.png" class="card-img-top img-fluid h-75" alt="..." />
+                            <div class="card text-dark card-media" style={{ width: "18rem", height: "400px" }}>
+                                <img src="https://i.ibb.co/WsNc6L5/ezgif-com-gif-maker-4.gif" class="card-img-top img-fluid h-75" alt="..." />
                                 <div class="card-body">
                                     <h3>Travel Destination</h3>
                                     <p class="card-text text-dark"><span style={{ color: "#ec255a" }}>Travel Destination</span> Travel Destination is a Full Stack Project. It is A tourism related website.</p>
@@ -28,8 +28,8 @@ const MyProjects = () => {
                             </div>
                         </Col>
                         <Col>
-                            <div class="card text-dark card-media" style={{ width: "18rem", height: "600px" }}>
-                                <img src="https://i.ibb.co/pJXxVx4/5-IC84-Ba-ETz-1-1.png" class="card-img-top img-fluid h-75" alt="..." />
+                            <div class="card text-dark card-media" style={{ width: "18rem", height: "400px" }}>
+                                <img src="https://i.ibb.co/KrN1gXM/ezgif-com-gif-maker-5.gif" class="card-img-top img-fluid h-75" alt="..." />
                                 <div class="card-body">
                                     <h3>Royal Enfield</h3>
                                     <p class="card-text text-dark"><span style={{ color: "#ec255a" }}>Royal Enfield</span> Royal Enfield is a Full Stack Project. It is A bike showroom's website.</p>
@@ -42,22 +42,22 @@ const MyProjects = () => {
                             </div>
                         </Col>
                         <Col>
-                            <div class="card text-dark card-media" style={{ width: "18rem", height: "600px" }}>
-                                <img src="https://i.ibb.co/B4c48Q6/Wuo-Q8g-Zunc.png" class="card-img-top img-fluid h-75" alt="..." />
+                            <div class="card text-dark card-media" style={{ width: "18rem", height: "400px" }}>
+                                <img src="https://i.ibb.co/PQDGBTK/ezgif-com-gif-maker-7.gif" class="card-img-top img-fluid h-100" alt="..." />
                                 <div class="card-body">
-                                    <h3>Daily -Health- Care</h3>
-                                    <p class="card-text text-dark"><span style={{ color: "#ec255a" }}>Daily -Health- Care</span> Daily Health Care is a Front End Project. It is A health care related clinic website.</p>
+                                    <h3>Apartment Seller</h3>
+                                    <p class="card-text text-dark"><span style={{ color: "#ec255a" }}>Apartment Seller</span> Apartment Seller is a Full-Stack Project. It is a selling or buying appertment website.</p>
                                     <div className="d-flex justify-content-evenly">
                                         <a href="https://daily-health-care-1a683.web.app/home"><button className="btn btn-info rounded-pill text-white">Preview</button></a>
 
-                                        <Link to="/dailyHealthCare"><button className="btn btn-dark rounded-pill">Details</button></Link>
+                                        {/* <Link to="/dailyHealthCare"><button className="btn btn-dark rounded-pill">Details</button></Link> */}
                                     </div>
                                 </div>
                             </div>
                         </Col>
                         <Col>
-                            <div class="card text-dark card-media" style={{ width: "18rem", height: "600px" }}>
-                                <img src="https://i.ibb.co/FgMW8jF/76j-UXk8-Fl7.jpg" class="card-img-top img-fluid h-75" alt="..." />
+                            <div class="card text-dark card-media" style={{ width: "18rem", height: "400px" }}>
+                                <img src="https://i.ibb.co/JnzcDnS/ezgif-com-gif-maker-6.gif" class="card-img-top img-fluid h-75" alt="..." />
                                 <div class="card-body">
                                     <h3>Nest</h3>
                                     <p class="card-text text-dark"><span style={{ color: "#ec255a" }}>Nest</span>It is a e-commerce website you can buy all of you want.</p>
@@ -70,6 +70,21 @@ const MyProjects = () => {
                             </div>
                         </Col>
                     </Col>
+
+                    <Col>
+                        <div class="card text-dark card-media" style={{ width: "18rem", height: "400px" }}>
+                            <img src="https://i.ibb.co/3WwPMzP/ezgif-com-gif-maker-1.gif" class="card-img-top img-fluid h-75" alt="..." />
+                            <div class="card-body">
+                                <h3>Brain Skill</h3>
+                                <p class="card-text text-dark"><span style={{ color: "#ec255a" }}>Brain Skill</span> Brain Skill is a Full Stack Project. It is E-Learning website.</p>
+                                <div className="d-flex justify-content-evenly">
+                                    <a href="https://brain-skill.netlify.app/"><button className="btn btn-info text-white rounded-pill">Preview</button></a>
+                                    {/* <Link to="/travelDestination"><button className="btn btn-dark rounded-pill">Details</button></Link> */}
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+
                 </Row>
             </Container>
         </div>
